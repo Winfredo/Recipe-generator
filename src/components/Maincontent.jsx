4dff01ccfbbf07929a6bb39f7e0f480f4f5515b4
winfredo <winfrednukpe2002@gmail.com>
2025-07-26 17:16:40 +0000

    adding states to get the value of the inputs and displaying it on the page.

--- src/components/Maincontent.jsx
+++ src/components/Maincontent.jsx
@@ -1,15 +1,32 @@
-
+import { useState } from "react";
 export default function Main() {
-    return (
-        <main>
-            <form className="add-ingredient-form">
-                <input 
-                    type="text"
-                    placeholder="e.g. oregano"
-                    aria-label="Add ingredient"
-                />
-                <button>Add ingredient</button>
-            </form>
-        </main>
-    )
+  const [ingredients, setIngredients] = useState([]);
+
+  let handleSubmit = (event) =>{
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const newIngredient = formData.get("ingredient");
+    setIngredients(prev => [...prev, newIngredient])
+  }
+
+  const ingredientListItems = ingredients.map((ingredient) => {
+    return <li key={ingredient}>{ingredient}</li>;
+  });
+
+  return (
+    <main>
+      <div className="main-content">
+        <form onSubmit={handleSubmit} className="add-ingredient-form">
+          <input
+            type="text"
+            placeholder="e.g. oregano"
+            aria-label="Add ingredient"
+            name="ingredient"
+          />
+          <button>Add ingredient</button>
+        </form>
+        <ul>{ingredientListItems}</ul>
+      </div>
+    </main>
+  );
 }
